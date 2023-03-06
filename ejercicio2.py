9fe9d60805cf0c0e785af29ebe5e83de923eae4f
Elio Duran <elioduran60@gmail.com>
2023-03-05 21:58:05 -0300

    Cambios varios en los  archivos ejerccio y kc

---
+++ ejercicio2.py
@@ -0,0 +1,11 @@
+#Ejercicio 2: Usar la funcion print y el comando salto de linea
+# Para producir exactamente la siguiente salida:
+print('*************Opción 1************:')
+s1='Erase un hombre a una nariz pegado, '
+s2='Erase una nariz superlativa, '
+s3='Erase una alquitara medio viva, '
+s4='Erase un peje espada mal malvado. ' 
+print( s1, '\n' + s2, '\n' + s3, '\n' + s4)
+# Otra opción sería:
+print('*************Opción 2************:')
+print('Erase un hombre a una nariz pegado,\nErase una nariz superlativa,\nErase una alquitara medio viva,\nErase un peje espada mal malvado. ')
